ebe1275b5ad302d0a038ba2d24bb68540ecc1729
Karthus77 <72655347+Karthus77@users.noreply.github.com>
2022-09-28 10:39:47 +0800

    update time

--- OUCAutoUpdate.py
+++ OUCAutoUpdate.py
@@ -1,11 +1,17 @@
 import requests
 import time
 import os
+from datetime import datetime, timezone, timedelta
+
+
+tz = timezone(timedelta(hours=+8))
+fmt = '%Y%m%d'
+zoned_time = datetime.today().astimezone(tz)
 
 Loginkey = os.environ["LOGINKEY"]
 Sendkey = os.environ["SENDKEY"]
 form_data = os.environ["FORM_DATA"]
-
+form_data = form_data + '&date='+str(zoned_time.strftime(fmt))+'&created='+str(int(time.time()))
 
 url = 'https://pingan.ouc.edu.cn/ncov/wap/default/save'
 
@@ -18,11 +24,11 @@ headers = {
 	'X-Requested-With':'XMLHttpRequest',
 	'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
 	}
+try:
+	r = requests.post(url,data = form_data,headers = headers,cookies = Cookies,verify=False)
+except UnicodeEncodeError:
+	r = requests.post(url,data = form_data.encode("utf-8"),headers = headers,cookies = Cookies,verify=False)
 
-r = requests.post(url,data = form_data,headers = headers,cookies = Cookies,verify=False)
-
-fturl = 'https://sctapi.ftqq.com/' + Sendkey + '.send?title= '+time.strftime("%Y-%m-%d %X", time.localtime())+'&desp='+r.text
-
-print(r.text)
+fturl = 'https://sctapi.ftqq.com/' + Sendkey + '.send?title= '+ str(r.json()["m"])
 
 requests.get(fturl)
